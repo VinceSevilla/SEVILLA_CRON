0e16cb0eb62eb693359fc87a5311f41762d2a5a2
Vince Sevilla <133684091+VinceSevilla@users.noreply.github.com>
2025-04-08 08:38:20 +0000

    Initial commit

--- log_time.sh
+++ log_time.sh
@@ -1,1 +1,7 @@
-echo "Current Date and Time: $(date)" >> log.txt
+#!/bin/bash
+# Append timestamp and username to log.txt
+echo "Current Date and Time: $(date) | User: $GITHUB_ACTOR" >> log.txt
+
+# Count total entries and update log_count.txt
+count=$(wc -l < log.txt)
+echo "Total log entries: $count" > log_count.txt
